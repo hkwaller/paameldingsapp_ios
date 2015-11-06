90e3bd426e98aacdb9b8d53fc8efeb33de7a5ad9
Hannes Waller <hannes.waller@bekk.no>
2015-11-06 09:38:20 +0100

    start project

---
+++ Paameldingsapp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Paameldingsapp
+//
+//  Created by Hannes Waller on 2015-11-06.
+//  Copyright © 2015 Hannes Waller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
