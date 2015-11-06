adcf1bff2cb371b72755d047dcc613dc7dbba10f
Hans-Christian Fjeldberg <hans-christian@fjeldbergconsulting.no>
2015-11-06 13:49:53 +0100

    Showing name based on downloaded events

--- Paameldingsapp/EventViewController.swift
+++ Paameldingsapp/EventViewController.swift
@@ -10,23 +10,38 @@ import Foundation
 import UIKit
 
 class EventViewController : UIViewController {
+
+    var eventIndex: Int?
+
     @IBOutlet weak var eventNameLabel: UILabel!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+
         renderLogo()
+
+        EventsService.instance.getUpcomingEvents { (status, events) in
+
+            guard let index = self.eventIndex, let actualEvents = events else {
+                print("Index or events not present, nothing to show")
+                return
+            }
+
+            let event = actualEvents[index]
+
+            self.eventNameLabel.text = event["subject"] as? String
+        }
     }
-    
+
     func renderLogo() {
         let logo = UIImage(named: "Logo")
-        
+
         let marginX = (self.navigationController!.navigationBar.frame.size.width / 2) - (logo!.size.width / 4);
         let imgView = UIImageView(frame: CGRectMake(marginX, 10, logo!.size.width / 2, logo!.size.height / 2))
-        
+
         imgView.image = logo
-        
+
         self.navigationController!.navigationBar.addSubview(imgView)
     }
 }
